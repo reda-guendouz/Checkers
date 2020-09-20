58784840215c5a2a21c6cebcb033830a17e25886
bahraoui <60045723+bahraoui@users.noreply.github.com>
2020-09-20 09:51:05 +0200

    mise a jour des conflits

--- code/controleur.c
+++ code/controleur.c
@@ -12,14 +12,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-	init_graphics(800,600); /* (largeur, hauteur) */
-	affiche_auto_off(); // desactivation de la mise a jour automatique des dessins/objets graphiques
-	creation_damier(); // Mise à disposition graphique du damier sur une plage de 500 par 500 (10x10 cases)
-	affiche_all(); // affichage du damier
-	wait_escape(); // attente de la touche "Echap afin de quitter l'application"
-=======
-
 	init_graphics(799,600); /* (largeur, hauteur) */
 	int fin = 1;
 	SDL_Event event;    // Creation d une entree clavier a recuperer
@@ -30,7 +22,7 @@ int main()
 			case SDL_KEYDOWN:
 				switch (event.key.keysym.sym){
 					case SDLK_UP:    // Si touche du haut => Damier
-						creation_damier();
+						creation_damier(); // Mise à disposition graphique du damier sur une plage de 500 par 500 (10x10 cases)
 						break;
 					case SDLK_DOWN:  // Si touche du bas => Parametre
 						printf("Parametre\n");
@@ -47,6 +39,5 @@ int main()
 		}
 	}
 	SDL_Quit();
->>>>>>> 781ae4bedec6d316aa4544b0cb02eecc09708bff
 	return(0);
 }
