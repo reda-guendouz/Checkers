42399a073c6a907e3542e3c965c9e748614aa52a
Marwane Bahraoui <bahraoui.marwane@gmail.com>
2020-09-20 10:06:54 +0200

    ajout commentaires

--- code/vue.c
+++ code/vue.c
@@ -29,9 +29,9 @@ void creation_damier(){
             {
                 dessiner_case(P1,25,gris); // dessiner case foncee
                 if (P1.y<200)
-                    draw_fill_circle(P1,23,blanc);
+                    draw_fill_circle(P1,23,blanc); // dessiner pion blanc
                 else if (P1.y>300)
-                    draw_fill_circle(P1,23,noir);                
+                    draw_fill_circle(P1,23,noir); // dessiner pion noir               
             }
         }
         
