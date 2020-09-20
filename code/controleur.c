781ae4bedec6d316aa4544b0cb02eecc09708bff
reda-guendouz <reda.guendouz777@gmail.com>
2020-09-20 09:34:55 +0200

    ajout commentaire + menu

--- code/controleur.c
+++ code/controleur.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
 #include "vue.c"
+#include<stdlib.h>
+#include<stdio.h>
+#include<SDL/SDL.h> 
+
 
 
 /****************************
@@ -9,8 +13,33 @@
 
 int main()
 {
-	init_graphics(800,600); /* (largeur, hauteur) */
-	creation_damier();
-	wait_escape();
+
+	init_graphics(799,600); /* (largeur, hauteur) */
+	int fin = 1;
+	SDL_Event event;    // Creation d une entree clavier a recuperer
+	while (fin) {
+		//printf("dedans\n");
+		SDL_WaitEvent(&event);  //Attend pendant 10 ms pour recuperer une entree
+		switch (event.type) {
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym){
+					case SDLK_UP:    // Si touche du haut => Damier
+						creation_damier();
+						break;
+					case SDLK_DOWN:  // Si touche du bas => Parametre
+						printf("Parametre\n");
+						break;
+					case SDLK_ESCAPE:// Si echap => QUIT
+						fin = 0;
+						break;
+					default :
+						break;
+				}
+				break;
+			default :
+				break;
+		}
+	}
+	SDL_Quit();
 	return(0);
 }
