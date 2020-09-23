7ac07a6aea8c82aa342218bb1a10c20ac5ca633e
reda-guendouz <reda.guendouz777@gmail.com>
2020-09-23 15:26:50 +0200

    new file

---
+++ code/controleur.c
@@ -0,0 +1,18 @@
+#include "graphics.h"
+#include "vue.c"
+#include "modele.c"
+#include<stdlib.h>
+#include<stdio.h>
+#include<SDL/SDL.h> 
+
+
+/****************************
+*            Main           *
+****************************/
+
+
+int main()
+{	
+	
+	
+}
---
+++ code/modele.c
@@ -0,0 +1,16 @@
+#include "modele.h"
+
+void init_tabDamier ()
+{
+}
+
+void test_afficheTab(){
+}
+
+
+void deplacement(numCase source, numCase destination){
+
+}	
+int est_dans_la_zone(POINT p){
+ return 0;
+}
