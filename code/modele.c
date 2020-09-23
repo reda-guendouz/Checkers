76b9d386bde622abdda555ab515358fd0c85e1f1
Bahraoui Marwane <bahraoui.marwane@gmail.com>
2020-09-23 10:44:05 +0200

    fct est_dans_la_zone

--- code/modele.c
+++ code/modele.c
@@ -46,3 +46,11 @@ int deplacement(numCase source, numCase destination){
 	tableau[source.c][source.l] = 0;
 	return 0;
 }
+
+int est_dans_la_zone(POINT p){
+	if (p.x < 500 && p.y < 500)
+	{
+		return 1;
+	}
+	return 0;
+}
--- code/modele.h
+++ code/modele.h
@@ -21,3 +21,5 @@ void test_afficheTab();
 numCase clic_to_numCase(POINT p);
 
 int deplacement(numCase source, numCase destination);
+
+int est_dans_la_zone(POINT p);
