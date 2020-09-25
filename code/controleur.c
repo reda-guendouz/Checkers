5a400322cf7e7da9d1a9375a16beb808541ffce4
reda-guendouz <reda.guendouz777@gmail.com>
2020-09-25 10:58:39 +0200

    Premiers deplacements

--- code/controleur.c
+++ code/controleur.c
@@ -41,11 +41,12 @@ int main()
 	init_tabDamier();
 	//test_afficheTab();
 
-	//affiche_damier_classique();
-	affiche_damier_alternatif();
+	affiche_damier_classique();
+	//affiche_damier_alternatif();
 	deplacement(nc1,nc2);
 	wait_clic();
-	affiche_damier_alternatif();
+	affiche_damier_classique();
+	//affiche_damier_alternatif();
 	wait_escape();
 	return 0;
 	
