ae09efa566f051d15363bba87980b37ca01a2b63
bahraoui marwane <bahraoui.marwane@gmail.com>
2020-09-27 20:31:31 +0200

    debut d'une reel mise en page du controleur

--- code/controleur.c
+++ code/controleur.c
@@ -11,9 +11,13 @@
 
 
 /****************************
-*            Main           *
+*            FONCTIONS      *
 ****************************/
 
+BOOL joueur_peut_jouer(COULP joueur){
+	// verification qu'au moins une des cases du joueur peut bouger
+	return false;
+}
 POINT numCase_to_point_classique(numCase nc){
 	POINT p;
 	p.x = (nc.c*TAILLE_CASE)+ 325;
@@ -42,13 +46,20 @@ numCase clic_to_numCase_alternatif(POINT p){
 	return nc;
 }
 
+/****************************
+*            Main           *
+****************************/
+
+
 int main()
 {	
+	// Variables
+	BOOL jeu_fini=false;
+	COULP joueur_actuel = NOIR; // le premier joueur est celui possédant les pions noirs
+
 	numCase nc1,nc2;
-	nc1.c = 3;
-	nc1.l = 2;
-	nc2.c = 4;
-	nc2.l = 1;
+	nc1.c = 3; nc1.l = 2;
+	nc2.c = 4; nc2.l = 1;
 	init_graphics(LARGEUR_FENETRE,HAUTEUR_FENETRE);
 	affiche_auto_off();
 	fill_screen(blanc);
@@ -65,13 +76,21 @@ int main()
 	POINT p1;
 	p1 = numCase_to_point_classique(nc1);
 	//p1 = numCase_to_point_alternatif(nc1);
-	printf("%d %d\n",p1.x, p1.y);
-	printf("%d %d\n",p2.x, p2.y);
+	printf("point p1 : %d %d\n",p1.x, p1.y);
+	printf("point p2 : %d %d\n",p2.x, p2.y);
 	//affiche_damier_classique();
 	//affiche_damier_alternatif();
 	PIECE P = tableau[nc1.l][nc1.c];
 	affiche_deplacement_piece_ronde(P,p1,p2);
 	//affiche_deplacement_piece_losange(P,p1,p2);
+
+	while (jeu_fini)
+	{
+		// jeu
+		jeu_fini = joueur_peut_jouer();
+	}
+	
+
 	wait_escape();
 	return 0;
 	
