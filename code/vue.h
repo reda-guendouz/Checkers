7b782d9fe99c64a2e27bba523b4bf305497f3fb1
reda-guendouz <reda.guendouz777@gmail.com>
2020-09-28 14:02:59 +0200

    Ajout de la vue dans le master

--- code/vue.h
+++ code/vue.h
@@ -1,17 +1,54 @@
-#define RAYON 25
+#define RAYON 22
 #define TAILLE_CASE 50
 
-void affiche_piece_ronde(PIECE P, POINT p);
-void affiche_piece_losange(PIECE P, POINT p);
+typedef enum{
+    CLASSIQUE,ALTERNATIF
+}DAMIER_CHOIX;
 
-void affiche_case_carre(POINT p1,POINT p2, COULEUR clr);
-void affiche_case_rond(POINT p, COULEUR clr);
 
-void efface_piece_ronde(POINT p);
-void efface_piece_losange(POINT p);
 
-void affiche_deplacement_piece_ronde(PIECE P,POINT p1, POINT p2);
-void affiche_deplacement_piece_losange(PIECE P,POINT p1, POINT p2);
+/*******************************
+*      Affichage de piece      *
+*******************************/
 
-void affiche_damier_classique();
-void affiche_damier_alternatif();
+void affiche_piece_ronde(PIECE P, POINT p, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+void affiche_piece_losange(PIECE P, POINT p,COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+/*******************************
+*       Affiche case           *
+*******************************/
+
+void affiche_case_carre(POINT p1, POINT p2, COULEUR clr);
+
+void affiche_case_ronde(POINT p, COULEUR clr);
+
+/*******************************
+*       Effacer une piece      *
+*******************************/
+
+void efface_piece_ronde(POINT p,COULEUR clr);
+
+void efface_piece_losange(POINT p, COULEUR clr);
+
+/*******************************
+*      Deplacement piece       *
+*******************************/
+
+void affiche_deplacement_piece_ronde(PIECE P,POINT p1, POINT p2, COULEUR clr_case_sombre, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+void affiche_deplacement_piece_losange(PIECE P,POINT p1, POINT p2, COULEUR clr_case_sombre, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+/*******************************
+*        Affiche damier        *
+*******************************/
+
+void affiche_damier_classique(COULEUR clr_case_claire, COULEUR clr_case_sombre, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+void affiche_damier_alternatif(COULEUR clr_case_claire, COULEUR clr_case_sombre, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
+
+/*******************************
+*        Affiche Plateau       *
+*******************************/
+
+void affiche_plateau(DAMIER_CHOIX choix, COULEUR clr_case_claire, COULEUR clr_case_sombre, COULEUR clr_pion_clair, COULEUR clr_pion_sombre);
