deadd8974ee1014c11d2d23e28eb498c148c30c3
reda-guendouz <reda.guendouz777@gmail.com>
2020-09-28 14:10:18 +0200

    controleur du boss

--- code/controleur.c
+++ code/controleur.c
@@ -11,13 +11,9 @@
 
 
 /****************************
-*            FONCTIONS      *
+*            Main           *
 ****************************/
 
-BOOL joueur_peut_jouer(COULP joueur){
-	// verification qu'au moins une des cases du joueur peut bouger
-	return false;
-}
 POINT numCase_to_point_classique(numCase nc){
 	POINT p;
 	p.x = (nc.c*TAILLE_CASE)+ 325;
@@ -46,28 +42,17 @@ numCase clic_to_numCase_alternatif(POINT p){
 	return nc;
 }
 
-/****************************
-*            Main           *
-****************************/
-
-
 int main()
 {	
-	// Variables
-	BOOL jeu_fini=false;
-	COULP joueur_actuel = NOIR; // le premier joueur est celui possédant les pions noirs
-
 	numCase nc1,nc2;
-	nc1.c = 3; nc1.l = 2;
-	nc2.c = 4; nc2.l = 1;
+	nc1.c = 3;
+	nc1.l = 2;
+	nc2.c = 4;
+	nc2.l = 1;
 	init_graphics(LARGEUR_FENETRE,HAUTEUR_FENETRE);
 	affiche_auto_off();
-	fill_screen(blanc);
 	init_tabDamier();
-	//test_afficheTab();
-	/*
-	affiche_damier_classique();
-	//affiche_damier_alternatif();
+	affiche_plateau(CLASSIQUE,argent,gris,blanc,darkblue);
 	wait_clic();
 	deplacement(nc1,nc2);
 	POINT p2;
@@ -76,21 +61,9 @@ int main()
 	POINT p1;
 	p1 = numCase_to_point_classique(nc1);
 	//p1 = numCase_to_point_alternatif(nc1);
-	printf("point p1 : %d %d\n",p1.x, p1.y);
-	printf("point p2 : %d %d\n",p2.x, p2.y);
-	//affiche_damier_classique();
-	//affiche_damier_alternatif();
 	PIECE P = tableau[nc1.l][nc1.c];
-	affiche_deplacement_piece_ronde(P,p1,p2);
-	//affiche_deplacement_piece_losange(P,p1,p2);
-	*/
-	while (jeu_fini)
-	{
-		// jeu
-		jeu_fini = joueur_peut_jouer();
-	}
-	
-
+	affiche_deplacement_piece_ronde(P,p1,p2,gris,blanc,darkblue);
+	//affiche_deplacement_piece_losange(P,p1,p2,gris,blanc,noir);
 	wait_escape();
 	return 0;
 	
